83bfa7314027f84720b0e50427a71d5cb475d683
azure <ppcardana@hotmail.com>
2024-10-10 15:50:10 -0300

    projeto

---
+++ projeto_composicao/cubo.h
@@ -0,0 +1,10 @@
+#ifndef CUBO_H
+#define CUBO_H
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/freeglut_std.h>
+
+void DesenharCubo();
+
+#endif //CUBO_H
---
+++ projeto_composicao/esfera.h
@@ -0,0 +1,9 @@
+#ifndef ESFERA_H
+#define ESFERA_H
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+void DesenharEsfera(GLuint texture);
+
+#endif //ESFERA_H
