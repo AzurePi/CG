82b98c8d1badc5406e3c433b56a90f464f6cf142
azure <ppcardana@hotmail.com>
2024-11-06 00:40:49 -0300

    finalizando o projeto de transformaçõess

--- transformações/bules.c
+++ transformações/bules.c
@@ -4,7 +4,6 @@
 #include <GL/glut.h>
 
 GLfloat AspectRatio;
-GLfloat fazCalculoPonto;
 
 
 // Inicializa os parâmetros globais de OpenGL e do cenário
@@ -20,31 +19,78 @@ void init(void) {
 void PosicUser() {
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-
+    gluPerspective(45, AspectRatio, 1, 1000);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
+    gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
 }
 
 
 // trata o redimensionamento da janela OpenGL
 void reshape(int w, int h) {
-    if (h == 0)
-        h = 1;
-
+    if (h == 0) h = 1;
     AspectRatio = 1.0f * w / h;
+    glViewport(0, 0, w, h);
     PosicUser();
 }
 
 
 void DesenhaCenario() {
+    float size = 0.6;
+
+    // Primeiro bule
     glPushMatrix();
+    glTranslatef(-2.6f, 0.0f, 0.0f);
+    glRotatef(0, 1, 0, 0);
+    glRotatef(0, 0, 1, 0);
+    glRotatef(0, 0, 0, 1);
 
     glColor3f(0, 0.5, 1);
-    glutSolidTeapot(0.3);
+    glutWireTeapot(size);
+    glColor3f(1, 0.5, 0);
+    glutSolidTeapot(size);
+
+    glPopMatrix();
+
+    // Segundo bule
+    glPushMatrix();
+    glTranslatef(-0.85f, 0.0f, 0.0f);
+    glRotatef(0, 1, 0, 0);
+    glRotatef(90, 0, 1, 0);
+    glRotatef(0, 0, 0, 1);
 
     glColor3f(0, 0.5, 1);
-    glTranslatef(3, 0, 0);
-    glutSolidTeapot(0.3);
+    glutWireTeapot(size);
+    glColor3f(1, 0.5, 0);
+    glutSolidTeapot(size);
+
+    glPopMatrix();
+
+    // Terceiro bule
+    glPushMatrix();
+    glTranslatef(0.6f, 0.0f, 0.0f);
+    glRotatef(0, 1, 0, 0);
+    glRotatef(00, 0, 1, 0);
+    glRotatef(90, 0, 0, 1);
+
+    glColor3f(0, 0.5, 1);
+    glutWireTeapot(size);
+    glColor3f(1, 0.5, 0);
+    glutSolidTeapot(size);
+
+    glPopMatrix();
+
+    // Quarto bule
+    glPushMatrix();
+    glTranslatef(2.4f, 0.0f, 0.0f);
+    glRotatef(90, 1, 0, 0);
+    glRotatef(0, 0, 1, 0);
+    glRotatef(0, 0, 0, 1);
+
+    glColor3f(0, 0.5, 1);
+    glutWireTeapot(size);
+    glColor3f(1, 0.5, 0);
+    glutSolidTeapot(size);
 
     glPopMatrix();
 }
@@ -52,32 +98,18 @@ void DesenhaCenario() {
 
 void display(void) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    glViewport(0, 0, 600, 500);
-
     PosicUser();
     DesenhaCenario();
-
     glutSwapBuffers();
 }
 
 void keyboard(unsigned char key) {
-    switch (key) {
-    case 27: exit(0);
-    case ' ': fazCalculoPonto = 1;
-        break;
-    default: break;
-    }
+    if (key == 27) exit(0);
 }
 
 void arrow_keys(int a_keys) {
-    switch (a_keys) {
-    case GLUT_KEY_UP: glutFullScreen();
-        break;
-    case GLUT_KEY_DOWN: glutInitWindowSize(600, 500);
-        break;
-    default: break;
-    }
+    if (a_keys == GLUT_KEY_UP) glutFullScreen();
+    if (a_keys == GLUT_KEY_DOWN) glutReshapeWindow(600, 500);
 }
 
 
@@ -86,7 +118,7 @@ int main(int argc, char **argv) {
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGB);
 
     glutInitWindowPosition(0, 0);
-    glutInitWindowSize(600, 500);
+    glutInitWindowSize(900, 500);
     glutCreateWindow("Projeto CG - Bule");
 
     init();
